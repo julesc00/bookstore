38e0fa07761c9b9a03d7a3186397b6d4b9189979
Julio Briones <juliobriones@pop-os.localdomain>
2024-05-21 21:21:13 -0600

    WIP

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("pages.urls")),
     path("users/", include("users.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,5 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
+    """Left at page 92 of the book"""
     template_name = "pages/home.html"
